adb70bc2b7ece9a6bed4929a102a64ef8c7b108e
jackraja <raja@caiplanet.in>
2022-11-09 17:40:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>Hello world from PHP! Declarative pipeline!thanks for watching!</p>'; ?> 
+ <?php echo '<p>Hello world from PHP! Declarative pipeline!thanks for watching3!</p>'; ?> 
  </body>
 </html>
